4cb09dc3939b322b1760e985d6dcc32f20dca762
Mr. Robot <mr.robot@Mrs-MacBook-Air.local>
2026-07-22 18:12:54 -0400

    Use leadership header image

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "7px-hub-v2";
+const CACHE_NAME = "7px-hub-v3";
 const ASSETS = [
   "./",
   "index.html",
@@ -7,6 +7,7 @@ const ASSETS = [
   "manifest.webmanifest",
   "assets/icon-192.png",
   "assets/icon-512.png",
+  "assets/Header.PNG",
   "assets/rule-event-cards.png"
 ];
 
